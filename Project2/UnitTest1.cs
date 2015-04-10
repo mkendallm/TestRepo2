3f50f82c0ef12d50f4934a967b273edfaa2ba56a
Kendall <kendall@autoloop.net>
2015-04-10 16:08:08 -0400

    First Commit

---
+++ Project2/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Project2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine("Ya Ya Ya!");
+        }
+    }
+}
